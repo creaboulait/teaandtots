8e2792c1e923dd003076425fe1800e12d643a733
creaboulait <norshida.razali@gmail.com>
2018-08-07 16:05:30 +0800

    Add kids MVC

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,11 +1,15 @@
 class KidsController < ApplicationController	
+
 	def new
 		@kid = Kid.new
 	end
 
 	def create
 		@kid = Kid.new(kid_params)
+		#Hardcoding this at the mo, until I figure out sessions
+		@kid.user_id = 2
 		@kid.save
+		redirect_to user_path(@kid.user_id)
 	end
 
 	private

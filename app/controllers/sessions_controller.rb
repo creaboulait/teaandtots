447e2a38d4d13636f6c5b7e88f3a585c45bd9f22
creaboulait <norshida.razali@gmail.com>
2018-08-07 11:33:38 +0800

    Add styling to welcoem index

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_path
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  root 'welcome#index'
+  
+  get "/sign_up" => "users#new", as: "sign_up"
+  get "/sign_in" => "sessions#new", as: "sign_in"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
 end

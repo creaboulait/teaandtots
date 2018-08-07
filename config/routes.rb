8a255344017a9eae5ec3f95c6f921d8e6bd36e03
creaboulait <norshida.razali@gmail.com>
2018-08-07 11:00:31 +0800

    Add welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  
+  # delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "users#new", as: "sign_up"
+  get "/sign_in" => "sessions#new", as: "sign_in"
+
+  root 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2018_08_07_015627) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.string "password_digest"
     t.string "first_name"
     t.string "last_name"

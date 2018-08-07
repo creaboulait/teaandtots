d676e5fc780c70a0242930c9242017b5fd6f0afc
creaboulait <norshida.razali@gmail.com>
2018-08-07 12:45:09 +0800

    Add sessions VC

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,13 @@
 class SessionsController < ApplicationController
 
 	def create
-		sign_in(@user)
-		redirect_to root_path
+		@user = User.find_by(email: params[:session][:email])
+		if @user && @user.authenticate(params[:session][:password])
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
+		else
+			render 'sessions/new'
+		end
 	end
 
 	def destroy

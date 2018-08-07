f5db1bdc060b0099946db6d6ad20084479b6e6b3
creaboulait <norshida.razali@gmail.com>
2018-08-07 10:09:23 +0800

    Create users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+	def create
+		@user = User.new(user_params)
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :password, :first_name, :last_name, :profilepic)
+	end
 end

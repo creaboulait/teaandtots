7dda85ba388553dc9b4c88918359a04885eecd57
creaboulait <norshida.razali@gmail.com>
2018-08-07 16:44:11 +0800

    Add role column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_051556) do
+ActiveRecord::Schema.define(version: 2018_08_07_084245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_08_07_051556) do
     t.string "profilepic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
   end
 
 end

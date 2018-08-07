e6808a9a0e7f209e45c304d167623a05da92a888
creaboulait <norshida.razali@gmail.com>
2018-08-07 11:19:20 +0800

    Remove password from uses table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+
+  root 'welcome#index'
+  
+  get "/sign_up" => "users#new", as: "sign_up"
+  get "/sign_in" => "sessions#new", as: "sign_in"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
 end

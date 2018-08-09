624b1eaa2b9654c8c06a8d4554862b466aae193e
creaboulait <norshida.razali@gmail.com>
2018-08-09 11:14:27 +0800

    Complete Review Controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ApplicationRecord
-	validates :title, presence: true
-	validates :description, presence: true
-	validates :stars, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5,  only_integer: true }
 
 	belongs_to :user
 	belongs_to :playdate
+
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :stars, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5,  only_integer: true }
 end

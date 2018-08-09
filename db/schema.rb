ba8f63fb1f437692585bffe139d7e93948bca1d5
creaboulait <norshida.razali@gmail.com>
2018-08-09 17:24:18 +0800

    Add User index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_045937) do
+ActiveRecord::Schema.define(version: 2018_08_09_084831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2018_08_09_045937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.text "bio"
   end
 
 end
